d512185a60afe673bc9b858548b867fa5fe44f7b
Gopal Singhal <gopalsi@thoughtworks.com>
2018-11-26 09:59:19 +0530

    Add close for watir browser

--- watir.rb
+++ watir.rb
@@ -1,6 +1,16 @@
 require 'watir'
+require 'pry'
 
-browser = Watir::Browser.new # Opens chrome browser by default
-#browser = Watir::Browser.new :firefox
+#browser = Watir::Browser.new # Opens chrome browser by default
+browser = Watir::Browser.new :firefox
 
 browser.goto "https://www.google.com"
+
+puts browser.title
+
+browser.refre
+
+binding.pry
+
+
+browser.close
